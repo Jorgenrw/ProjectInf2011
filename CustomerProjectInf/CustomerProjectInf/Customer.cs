588dc1abcb10468bcdd80fe0cbd8d4639aa63beb
Jorgen Rugelsjoen Wikdahl <jorgenrw@stud.ntnu.no>
2014-10-13 15:40:00 +0200

    Making navigation from pages

---
+++ CustomerProjectInf/CustomerProjectInf/Customer.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomerProjectInf
+{
+    public class Customer
+    {
+        private int Id;
+        private string name;
+        private int phone;
+        private string Adress;
+
+        public Customer(int id, string name, int phone, string adress)
+        {
+            this.Id = id;
+            this.name = name;
+            this.phone = phone;
+            this.Adress = adress;
+        }
+        public int customerId
+        {
+            get
+            {
+                return Id;
+            }
+            set
+            {
+                //set customer Id here
+            }
+        }
+        public string customerName
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                //set customer name here
+            }
+        }
+        public int customerPhone
+        {
+            get
+            {
+                return phone;
+            }
+            set
+            {
+                // set customer phone here
+            }
+        }
+        public string customerAdress
+        {
+            get
+            {
+                return Adress;
+            }
+            set
+            {
+                // set customer address here
+            }
+        }
+        
+    }
+}
